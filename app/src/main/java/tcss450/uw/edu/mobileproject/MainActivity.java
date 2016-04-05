551ab31ca81575929b2ea3c7d7d2443598fffdac
khoadoan91 <kdoan91@uw.edu>
2016-04-05 15:19:54 -0700

    2nd commit from second team member

--- app/src/main/java/tcss450/uw/edu/mobileproject/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/mobileproject/MainActivity.java
@@ -1,5 +1,6 @@
 package tcss450.uw.edu.mobileproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void switchActivity() {
+        Intent intent = new Intent(this, SecondMemberActivity.class);
+        startActivity(intent);
     }
 }
