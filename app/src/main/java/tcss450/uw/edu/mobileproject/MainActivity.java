24d3e90321c0f4003ec280e5f730c7913056117a
shiweiwei0114 <wshi@uw.edu>
2016-04-29 14:52:33 -0700

    modified gradle

--- app/src/main/java/tcss450/uw/edu/mobileproject/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/mobileproject/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Comments for git lab use. 
+    //Comments for git lab use.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
